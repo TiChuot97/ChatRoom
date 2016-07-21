63e60d18450271cca834f3384af7c8cfcd04a6cb
TiChuot97 <daohoanglan2341997@gmail.com>
2016-07-21 12:01:44 +0700

    21 July 2016

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import Queue
 import threading
+from threading import Thread
 
 PACKAGE_LEN = 300
 MESSAGE_LEN = 290
@@ -28,6 +29,8 @@ def read_bytes_from_socket(num_bytes, socket):
     return (package, False)
 
 def process_client(client_socket, num_client):
+    #print "OK"
+    sys.stdout.flush()
     global queue
     while True:
         (user_name, error) = read_bytes_from_socket(USERNAME_LEN, client_socket)
@@ -73,11 +76,14 @@ client_connected = [1 for x in range(101)]
 
 while True:
     (client_socket, address) = server_socket.accept()
+    print address
     if num_clients == MAX_CLIENTS:
         client_socket.close() 
     else:
-        new_thread = thread(target = process_client, args = (client_socket, num_clients + 1), daemon = True)
+        new_thread = Thread(target = process_client, args = (client_socket, num_clients + 1))
+        new_thread.setDaemon(True)
+        new_thread.start()
         ++num_clients
-        client_sockets.append(client_socket, num_clients)
+        client_sockets.append((client_socket, num_clients))
 
 
