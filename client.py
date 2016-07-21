57b519c99856edc02971b70957aa12965d19e454
TiChuot97 <daohoanglan2341997@gmail.com>
2016-07-21 17:12:43 +0700

    21 July 2016

--- client.py
+++ client.py
@@ -4,12 +4,12 @@ from error_window import ErrorWindow
 from threading import Thread
 import socket
 
-
 PACKAGE_LEN = 300
 MESSAGE_LEN = 290
 USERNAME_LEN = 10
+CONNECTION_ERROR = "CONN_ERR"
 
-connection = True
+connected = True
 running = True
 
 def clean_up():
@@ -25,20 +25,41 @@ def clean_up():
     thread_2.join()
     chat_window.root.destroy()
 
+    client_socket.close()
+
 def connection_error():
     error_message = ErrorWindow("Connection lost!!!\nRestart the program!!!")
-    temp_thread = Thread(target = clean_up, args = ())
-    temp_thread.join()
+    error_message.root.mainloop()
 
-def send_to_server(chat_window):
-    global running, connection
+def write_bytes_to_socket(num_bytes, socket, message):
+    bytes_sent = 0
+    while bytes_sent < num_bytes:
+        sent = socket.send(message[bytes_sent:])
+        if sent == 0:
+            return True
+        bytes_sent += sent
+    return False
 
-    while running and connection:
+def read_bytes_from_socket(num_bytes, socket):
+    bytes_read = 0
+    package = ""
+    while bytes_read < num_bytes:
+        piece = socket.recv(num_bytes - bytes_read)
+        if len(piece) == 0:
+            return (CONNECTION_ERROR, True)
+        package = package + piece
+        bytes_read += len(piece)
+    return (package, False)
+
+def send_to_server():
+    global running, connected, chat_window, client_socket
+
+    while running and connected:
         chat_window.cv.acquire()
         while chat_window.num_queue == 0 and running == True:
             chat_window.cv.wait()
 
-        if not running or not connection:
+        if not running or not connected:
             return
 
         message = chat_window.message_queue.get()
@@ -55,33 +76,36 @@ def send_to_server(chat_window):
             message = message + " "
 
         # Message sending
-        byte_sent = 0
-        while (byte_sent < PACKAGE_LEN):
-            sent = client_socket.send(message[byte_sent:])
-            if sent == 0:
-                connection_error()
-                connection = False
-                return
-            byte_sent += sent
+        error = write_bytes_to_socket(PACKAGE_LEN, client_socket, message)
+        if error:
+            connection_error()
+            connected = False
+            return
 
         chat_window.cv.release()
 
 def receive_from_server():
-    global running, connection
+    global running, connected, chat_window, client_socket
     
-    while running and connection:
-        byte_read = 0
-        package = ""
-        if not running or connection:
+    while running and connected:
+        if not running or not connected:
             return
-        while byte_read < PACKAGE_LEN:
-            piece = client_socket.recv(PACKAGE_LEN - byte_read)
-            if len(piece) == 0:
-                connection_error()
-                connection = False
-                return
-            package = package + piece
-            byte_read += len(piece)
+
+        # Parse username
+        (user_name, error) = read_bytes_from_socket(USERNAME_LEN, client_socket) 
+        if error:
+            connection_error()
+            connected = False
+            return
+
+        # Parse message
+        (message, error) = read_bytes_from_socket(MESSAGE_LEN, client_socket)
+        if error:
+            connection_error()
+            connected = False
+            return
+
+        chat_window.displayChat(user_name + ": " + message)
     
 sign_in_window = SignInWindow()
 sign_in_window.root.mainloop()
@@ -96,7 +120,7 @@ client_socket.connect((host_name, int(port)))
 chat_window = ChatWindow(user_name)
 chat_window.root.protocol("WM_DELETE_WINDOW", clean_up)
 
-thread_1 = Thread(target = send_to_server, args = (chat_window, ))
+thread_1 = Thread(target = send_to_server, args = ())
 thread_2 = Thread(target = receive_from_server, args = ())
 
 thread_1.start()
